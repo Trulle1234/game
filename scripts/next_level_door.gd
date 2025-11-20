0bbf7895888c8df5ac533b33f1fd300390108f99
Trulle1234 <leonard.trulsson@gmail.com>
2025-11-20 19:12:05 +0100

    stuff works

---
+++ scripts/next_level_door.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var next_level_path = ""
+
+const FILE_BEGIN: String = "res://scenes/levels/level_"
+
+func _on_body_entered(body: Node2D) -> void:
+	var current_scene_file: String = get_tree().current_scene.scene_file_path
+	var next_level_number = current_scene_file.to_int() + 1
+	
+	next_level_path = FILE_BEGIN + str(next_level_number) + ".tscn"
+	
+	body.is_dead = true
+	body.velocity.x = 0
+	animated_sprite.play("closed")
+	body.get_node("AnimatedSprite2D").play("invis")
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	if FileAccess.file_exists(next_level_path):
+		get_tree().change_scene_to_file(next_level_path)
+	else:
+		get_tree().change_scene_to_file(FILE_BEGIN + "1.tscn")
--- scripts/player.gd
+++ scripts/player.gd
@@ -9,6 +9,8 @@ const GRAVITY = Vector2(0, 980.0)
 @onready var coyote_timer: Timer = $CoyoteTimer
 var coyote_time_active: bool = false
 
+@onready var jump_buffer_timer: Timer = $JumpBufferTimer
+
 var is_dead: bool = false
 
 func _physics_process(delta: float) -> void:
@@ -17,34 +19,39 @@ func _physics_process(delta: float) -> void:
 
 	var direction := Input.get_axis("left", "right")
 	
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
-	
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle")
-		else:
-			animated_sprite.play("run")
+	if not is_dead:
+		if direction > 0:
+			animated_sprite.flip_h = false
+		elif direction < 0:
+			animated_sprite.flip_h = true
 		
-		if coyote_time_active:
-			coyote_time_active = false
+		if is_on_floor():
+			if direction == 0:
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("run")
+			
+			if coyote_time_active:
+				coyote_time_active = false
+				coyote_timer.stop()
+		elif not is_on_floor():
+			animated_sprite.play("jump")
+			if not coyote_time_active:
+				coyote_timer.start()
+				coyote_time_active = true
+				
+		if Input.is_action_just_pressed("jump"):
+			jump_buffer_timer.start()
+
+			
+		if not jump_buffer_timer.is_stopped() and (not coyote_timer.is_stopped() or is_on_floor()):
+			velocity.y = JUMP_VELOCITY
 			coyote_timer.stop()
-	elif not is_on_floor() and not is_dead:
-		animated_sprite.play("jump")
-		if not coyote_time_active:
-			coyote_timer.start()
 			coyote_time_active = true
 			
-	if Input.is_action_just_pressed("jump") and (not coyote_timer.is_stopped() or is_on_floor()):
-		velocity.y = JUMP_VELOCITY
-		coyote_timer.stop()
-		coyote_time_active = true
-
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
